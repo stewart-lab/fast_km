32be8584435da0cf8919865fe270c86021261cb6
rmillikin <rmillikin@wisc.edu>
2021-10-06 11:16:41 -0500

    initial trie implementation

--- project/src/index.py
+++ project/src/index.py
@@ -1,43 +1,55 @@
 import math
+import os
+import pickle
 from project.src.database import Database
 
 class Index():
     def __init__(self, path_to_db: str):
-        self._ngrams_with_pmids = {}
-        self._publication_years = {}
-        self._indexed_filenames = []
-        self._db = Database(path_to_db)
-        self._indexed_filenames = self._db.get_indexed_abstracts_files()
-        self._publication_years = self._db.get_pub_years()
+        self._ngrams_with_pmids = dict()
+        self._publication_years = dict()
+        self._indexed_filenames = set()
+        self.path_to_db = path_to_db
+
+        if os.path.exists(path_to_db):
+            self._db = self._load_existing_trie()
+        else:
+            self._db = Database()
 
     def dump_cache_to_db(self):
         """Call this method while building the index periodically. Saves 
         info from in-memory dictionaries into the database and clears 
         in-memory dictionaries"""
-        self._db.dump_cache(self._ngrams_with_pmids, self._publication_years, self._indexed_filenames)
-        self._ngrams_with_pmids = {}
-        self._publication_years = {}
+        self._db.pickle_cache(self._ngrams_with_pmids, self._publication_years, self._indexed_filenames)
+        self._ngrams_with_pmids = dict()
+        self._publication_years = dict()
+        self._save_to_disk()
 
     def start_building_index(self):
-        self._ngrams_with_pmids = {}
-        self._publication_years = {}
+        self._ngrams_with_pmids = dict()
+        self._publication_years = dict()
 
     def finish_building_index(self):
         self.dump_cache_to_db()
-        self._db.combine_ngram_sets()
+        self._db.combine_pickled_sets()
+        self.dump_cache_to_db()
         
     def ngrams(self):
         return self._db.ngrams()
     
     def query_index(self, query: str) -> 'set[int]':
         """Returns a set of PMIDs that contain the query n-gram"""
-        if query in self._ngrams_with_pmids:
-            return self._ngrams_with_pmids[query.lower()]
+        query_lower = query.lower()
+        
+        if query_lower in self._ngrams_with_pmids:
+            return self._ngrams_with_pmids[query_lower]
         else:
-            return self._db.query(query.lower())
+            return self._db.query(query_lower)
 
     def get_publication_year(self, id: int) -> int:
-        return self._publication_years[id]
+        if id in self._publication_years:
+            return self._publication_years[id]
+        else:
+            return self._db.pub_years[id]
 
     def place_value(self, search_term: str, id: int, pub_year: int) -> None:
         lower_word = search_term.lower()
@@ -50,12 +62,12 @@ class Index():
     
     def n_articles(self, censor_year = math.inf) -> int:
         if censor_year == math.inf:
-            return len(self._publication_years)
+            return len(self._db.pub_years)
         else:
             num_articles_censored = 0
 
-            for year in self._publication_years:
-                if year <= censor_year:
+            for pmid in self._db.pub_years:
+                if self._db.pub_years[pmid] <= censor_year:
                     num_articles_censored += 1
         
             return num_articles_censored
@@ -84,3 +96,12 @@ class Index():
                     new_set.add(item)
 
         return new_set
+
+    def _load_existing_trie(self):
+        with open(self.path_to_db, 'rb') as handle:
+            b = pickle.load(handle)
+            return b
+        
+    def _save_to_disk(self):
+        with open(self.path_to_db, 'wb') as handle:
+            pickle.dump(self._db, handle)
--- project/tests/test_index.py
+++ project/tests/test_index.py
@@ -10,15 +10,21 @@ def test_index(tmp_path):
     pmid1 = 1
     pmid2 = 2
     pmid3 = 4
+    pmid4 = 5
     pub_year1 = 2020
     pub_year2 = 1984
     pub_year3 = 2019
+    pub_year4 = 2019
 
     the_index = Index(db_path)
     the_index.start_building_index()
     the_index.place_value("test", pmid1, pub_year1)
     the_index.place_value("test", pmid2, pub_year2)
     the_index.place_value("test2", pmid3, pub_year3)
+
+    the_index.dump_cache_to_db()
+
+    the_index.place_value("test", pmid4, pub_year4)
 
     # write the index to disk
     the_index.dump_cache_to_db()
@@ -29,11 +35,15 @@ def test_index(tmp_path):
     query = loaded_index.query_index("test")
     
     # check that index contains expected contents and can be queried
-    expected_set = set([pmid1, pmid2])
-    assert len(expected_set & query) == 2
+    expected_set = set([pmid1, pmid2, pmid4])
+    assert len(expected_set & query) == 3
     assert len(expected_set ^ query) == 0
     assert loaded_index.get_publication_year(pmid1) == pub_year1
     assert loaded_index.get_publication_year(pmid2) == pub_year2
+    assert loaded_index.get_publication_year(pmid3) == pub_year3
+    assert loaded_index.get_publication_year(pmid4) == pub_year4
+    assert loaded_index.n_articles() == 4
+    assert loaded_index.n_articles(censor_year=2000) == 1
 
     # query the index for an n-gram that doesn't exist
     empty_query = loaded_index.query_index("test_test")
