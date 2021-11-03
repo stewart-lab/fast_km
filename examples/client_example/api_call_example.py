e5c73deb21cf310baa55c7a72e05de7ae8b1dfeb
rmillikin <rmillikin@wisc.edu>
2021-11-03 10:39:07 -0500

    better demo script, resource limits

--- examples/client_example/api_call_example.py
+++ examples/client_example/api_call_example.py
@@ -4,8 +4,15 @@ import time
 # create the KM query details
 the_json = [
     { "a_term": "cancer", "b_term": "tumor" }, 
-    { "a_term": "cancer", "b_term": "test" }
+    { "a_term": "cancer", "b_term": "" }
 ]
+
+#with open('/path/to/my/gene/names/genes.txt', 'r') as file:
+#    genes = file.readlines()
+
+#the_json.clear()
+#for gene in genes:
+#    the_json.append({ "a_term": "cancer", "b_term": gene.strip() })
 
 api_url = 'http://localhost:5000/km/api/jobs'
 
@@ -17,22 +24,25 @@ json_post_response = response.json()
 job_id = json_post_response[0]['id']
 
 # get the status of the job
-get_response = requests.get(api_url + job_id)
+get_response = requests.get(api_url + "?id=" + job_id)
 json_get_response = get_response.json()
 job_status = json_get_response[0]['status']
 print('job status is: ' + job_status)
 
 # wait for job to complete, sleep
-if job_status == 'queued':
-    print('sleeping for 5 sec...')
-    time.sleep(5)
+approx_total_time = 0
+while job_status == 'queued' or job_status == 'started':
+    print('sleeping for 10 sec...')
+    time.sleep(10)
 
     # get the status of the job
-    get_response = requests.get(api_url + job_id)
+    get_response = requests.get(api_url + "?id=" + job_id)
     json_get_response = get_response.json()
     job_status = json_get_response[0]['status']
     print('job status is: ' + job_status)
+    approx_total_time += 10
 
 # if the job's status is 'finished', print out the results
 if job_status == 'finished':
     print(json_get_response[1]['result'])
+    print("total query time: " + str(approx_total_time) + " sec")
