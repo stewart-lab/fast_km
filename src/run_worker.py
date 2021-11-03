9e1f60981f35048c86e3305732f919a8115ce73a
rmillikin <rmillikin@wisc.edu>
2021-11-03 13:55:46 -0500

    num workers arg

--- src/run_worker.py
+++ src/run_worker.py
@@ -1,6 +1,7 @@
 import multiprocessing
 import os
 import time
+import argparse
 from workers.km_worker import start_worker
 import indexing.download_abstracts as downloader
 import indexing.index_abstracts as indexer
@@ -8,10 +9,16 @@ import workers.loaded_index as li
 from indexing.indexer import Indexer
 import indexing.disk_index as di
 
-n_workers = 3
-rebuild_index = False
+parser = argparse.ArgumentParser()
+parser.add_argument('-w', '--workers', default=1)
+parser.add_argument('-b', '--build_index', default=False)
+args = parser.parse_args()
 
 def start_workers(do_multiprocessing = True):
+    n_workers = args.workers
+
+    print('starting ' + str(n_workers) + ' workers...')
+
     if do_multiprocessing:
         jobs = []
         for i in range(0, n_workers):
@@ -26,6 +33,7 @@ def main():
     time.sleep(10)
     li.pubmed_path = '/mnt/pubmed'
 
+    rebuild_index = args.build_index
     build_index = rebuild_index or (not os.path.exists(li.flat_binary_path())) or (not os.path.exists(li.flat_text_path()))
 
     if build_index:
