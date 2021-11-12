2cbc6099c3906706abd8efef717d47ab8b32155b
rmillikin <rmillikin@wisc.edu>
2021-11-12 11:24:29 -0600

    init skim implementation

--- src/workers/work.py
+++ src/workers/work.py
@@ -2,7 +2,7 @@ import math
 import workers.loaded_index as li
 import workers.kinderminer as km
 
-def km_work(json):
+def km_work(json: list):
     return_val = []
 
     if len(json) > 100000:
@@ -50,3 +50,43 @@ def km_work(json):
             })
 
     return return_val
+
+def skim_work(json: dict):
+    return_val = []
+
+    a_terms = json['a_terms']
+    b_terms = json['b_terms']
+    c_terms = json['c_terms']
+    top_n = 50
+
+    significant_ab_queries = []
+    for a_term in a_terms:
+        for b_term in b_terms:
+            res = km.kinderminer_search(a_term, b_term, li.the_index, math.inf)
+            pvalue = res[4]
+            sort_ratio = res[5]
+
+            if pvalue < 1e10-5:
+                pred_score = -math.log10(max(pvalue, 10e-30)) + math.log10(sort_ratio)
+                significant_ab_queries.append((a_term, b_term, pred_score))
+
+    # sort by prediction score, descending
+    significant_ab_queries.sort(key=lambda query: query[2], reverse=True)
+
+    for ab in significant_ab_queries[:top_n]:
+        b_term = ab[1]
+        for c_term in c_terms:
+            res = km.kinderminer_search(b_term, c_term, li.the_index, math.inf)
+            return_val.append(
+                {
+                    'a_term': ab[0],
+                    'b_term': ab[1],
+                    'c_term': c_term,
+                    'bc_p-value': res[4],
+                    'ab_pred_score': ab[2]
+                })
+
+    return return_val
+
+def triple_miner_work(json):
+    pass
