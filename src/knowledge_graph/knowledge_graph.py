0c4114c716393ef9ce63283b3a1f3603e014cfea
Robert Millikin <rmillikin@wisc.edu>
2023-01-03 11:59:24 -0600

    implement synonyms and censor date in KG

--- src/knowledge_graph/knowledge_graph.py
+++ src/knowledge_graph/knowledge_graph.py
@@ -11,6 +11,7 @@ password = "mypass"
 
 rel_pvalue_cutoff = 1e-5
 min_pmids_for_rel = 3
+max_synonyms = 9999
 
 class KnowledgeGraph:
     def __init__(self):
@@ -32,27 +33,36 @@ class KnowledgeGraph:
             self.node_ids = dict()
             print('Problem loading graph node IDs')
 
-    def query(self, a_term: str, b_term: str):
+    def query(self, a_term: str, b_term: str, censor_year = None):
         if not self.graph:
             return [{'a_term': a_term, 'a_type': '', 'relationship': 'neo4j connection error', 'b_term': b_term, 'b_type': '', 'pmids': []}]
 
         if index.logical_and in a_term or index.logical_and in b_term:
             return [self._null_rel_response(a_term, b_term)]
 
-        a_term_stripped = _sanitize_txt(a_term)
-        b_term_stripped = _sanitize_txt(b_term)
+        a_term_stripped = _sanitize_txt(a_term)[:max_synonyms]
+        b_term_stripped = _sanitize_txt(b_term)[:max_synonyms]
 
-        if (a_term_stripped, b_term_stripped) in self.query_cache:
-            return self.query_cache[(a_term_stripped, b_term_stripped)]
+        if (str.join(index.logical_or, a_term_stripped), str.join(index.logical_or, b_term_stripped)) in self.query_cache:
+            return self.query_cache[str.join(index.logical_or, a_term_stripped), str.join(index.logical_or, b_term_stripped)]
 
         # get nodes from the neo4j database
+        a_matches = []
+        b_matches = []
         if self.node_ids:
-            a_matches = [self.graph.nodes.get(_id) for _id in self.node_ids.get(a_term_stripped, [])]
-            b_matches = [self.graph.nodes.get(_id) for _id in self.node_ids.get(b_term_stripped, [])]
+            for a_subterm in a_term_stripped:
+                if a_subterm in self.node_ids:
+                    _id = self.node_ids[a_subterm]
+                    a_matches.extend(self.graph.nodes.get(_id))
+            for b_subterm in b_term_stripped:
+                if b_subterm in self.node_ids:
+                    _id = self.node_ids[b_subterm]
+                    b_matches.extend(self.graph.nodes.get(_id))
         else:
             # this is ~50x slower than looking up by node ID but it will still work
-            a_matches = self.graph.nodes.match(name=a_term_stripped).all()
-            b_matches = self.graph.nodes.match(name=b_term_stripped).all()
+            # TODO: implement synonym searching? right now only searches first one
+            a_matches = self.graph.nodes.match(name=a_term_stripped[0]).all()
+            b_matches = self.graph.nodes.match(name=b_term_stripped[0]).all()
 
         # get relationship(s) between a and b nodes
         relation_matches = []
@@ -78,13 +88,34 @@ class KnowledgeGraph:
             
             relationship = str(type(relation)).replace("'", "").replace(">", "").split('.')[2]
 
-            relation_json = {'a_term': node1_name, 'a_type': node1_type, 'relationship': relationship, 'b_term': node2_name, 'b_type': node2_type, 'pmids':relation['pmids'][:100]}
+            if censor_year:
+                if censor_year not in li.the_index._date_censored_pmids:
+                    if not li.the_index._publication_years:
+                        li.the_index._init_pub_years()
+
+                    censored_set = set()
+
+                    for pmid, year in li.the_index._publication_years.items():
+                        if year <= censor_year:
+                            censored_set.add(pmid)
+                    li.the_index._date_censored_pmids[censor_year] = censored_set
+                else:
+                    censored_set = li.the_index._date_censored_pmids[censor_year]
+
+                pmids = list(set(relation['pmids']) & censored_set)
+
+                if not pmids:
+                    continue
+            else:
+                pmids = relation['pmids']
+
+            relation_json = {'a_term': node1_name, 'a_type': node1_type, 'relationship': relationship, 'b_term': node2_name, 'b_type': node2_type, 'pmids': pmids[:100]}
             result.append(relation_json)
 
         if not result:
             result.append(self._null_rel_response(a_term, b_term))
 
-        self.query_cache[(a_term_stripped, b_term_stripped)] = result
+        self.query_cache[str.join(index.logical_or, a_term_stripped), str.join(index.logical_or, b_term_stripped)] = result
         return result
 
     def write_node_id_index(self, path: str):
@@ -201,8 +232,11 @@ class KnowledgeGraph:
         {'a_term': a_term, 'a_type': '', 'relationship': '', 'b_term': b_term, 'b_type': '', 'pmids': []}
 
 def _sanitize_txt(term: str):
-    term = term.split(index.logical_or)[0]
-    return str.join(' ', util.get_tokens(term.lower().strip()))
+    subterms = set()
+    terms = term.split(index.logical_or)
+    for term in terms:
+        subterms.add(str.join(' ', util.get_tokens(term.lower().strip())))
+    return list(subterms)
 
 # batches "lst" into "chunk_size" sized elements
 def _group_elements(lst, chunk_size):
